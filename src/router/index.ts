478dcfeeb66acabc093b77b9a90dcbcb632b8b9d
csj <sjchen@gk-estor.com>
2021-12-15 15:50:11 +0800

    feat: 'dexie quickstart'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,60 +1,60 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import type { App } from 'vue';
 
-const routes:Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'index',
-        component: () => import('/@/views/test/Demo.vue')
-    },
-    {
-        path: '/check',
-        name: 'check',
-        component: () => import('/@/views/checkbox/index.vue')
-    },
-    {
-        path: '/table',
-        name: 'table',
-        component: () => import('/@/components/tableDataEngine.vue')
-    },
-    {
-        path: '/alex',
-        name: 'alex',
-        component: () => import('/@/views/user/alex/index.vue')
-    },
-    {
-        path: '/bill',
-        name: 'bill',
-        component: () => import('/@/views/user/bill/index.vue')
-    },
-    {
-        path: '/tom',
-        name: 'tom',
-        component: () => import('/@/views/user/tom/index.vue')
-    },
-    {
-        path: '/development',
-        name: 'development',
-        component: () => import('/@/views/team/development/index.vue')
-    },
-    {
-        path: '/testing',
-        name: 'testing',
-        component: () => import('/@/views/team/testing/index.vue')
-    },
-    {
-        path: '/file',
-        name: 'file',
-        component: () => import('/@/views/file/index.vue')
-    },
-]
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('/@/views/test/Demo.vue'),
+  },
+  {
+    path: '/check',
+    name: 'check',
+    component: () => import('/@/views/checkbox/index.vue'),
+  },
+  {
+    path: '/table',
+    name: 'table',
+    component: () => import('/@/components/tableDataEngine.vue'),
+  },
+  {
+    path: '/alex',
+    name: 'alex',
+    component: () => import('/@/views/user/alex/index.vue'),
+  },
+  {
+    path: '/bill',
+    name: 'bill',
+    component: () => import('/@/views/user/bill/index.vue'),
+  },
+  {
+    path: '/tom',
+    name: 'tom',
+    component: () => import('/@/views/user/tom/index.vue'),
+  },
+  {
+    path: '/development',
+    name: 'development',
+    component: () => import('/@/views/team/development/index.vue'),
+  },
+  {
+    path: '/testing',
+    name: 'testing',
+    component: () => import('/@/views/team/testing/index.vue'),
+  },
+  {
+    path: '/file',
+    name: 'file',
+    component: () => import('/@/views/file/index.vue'),
+  },
+];
 
 export const router = createRouter({
-    routes,
-    history: createWebHistory()
-})
+  routes,
+  history: createWebHistory(),
+});
 
 // config router
 export function setupRouter(app: App<Element>) {
-    app.use(router);
+  app.use(router);
 }
--- src/views/file/index.vue
+++ src/views/file/index.vue
@@ -1,5 +1,113 @@
-<template></template>
+<template>
+  <div>
+    <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
+      <a-form-item label="Name" v-bind="validateInfos.name">
+        <a-input v-model:value="state.name" />
+      </a-form-item>
+      <a-form-item label="Age" v-bind="validateInfos.age">
+        <a-input v-model:value="state.age" />
+      </a-form-item>
+      <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button type="primary" @click.prevent="onSubmit">Create</a-button>
+        <a-button class="ml-5" @click="reset">Reset</a-button>
+      </a-form-item>
+    </a-form>
+    <p>{{ state.status }}</p>
+    <a-table :dataSource="state.list" :columns="state.columns" />
+  </div>
+</template>
 
-<script lang="ts"></script>
+<script lang="ts" setup>
+  import { reactive, onMounted } from 'vue';
+  import { Form } from 'ant-design-vue';
+  import { db, Friend } from './dexie/index';
 
-<style scoped></style>
+  type List = Friend
+  interface Stete extends List{
+    status: string,
+    list: Friend[]
+  }
+
+  const state:Stete = reactive({
+    status: '',
+    name: '',
+    age: 18,
+    list: [],
+    columns: [
+      {
+        title: 'id',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+      },
+    ],
+  });
+
+  onMounted(() => {
+    initList()
+  })
+
+  const labelCol = { span: 4 };
+  const wrapperCol = { span: 14 };
+  const useForm = Form.useForm;
+  const { resetFields, validate, validateInfos } = useForm(
+    state,
+    reactive({
+      name: [
+        {
+          required: true,
+          message: 'Please input name',
+        },
+      ],
+      age: [
+        {
+          required: true,
+          message: 'Please input age',
+        },
+      ],
+    })
+  );
+
+  const onSubmit = () => {
+    validate().then(async (res) => {
+      try {
+        // Add the new friend!
+        const id = await db.friends.add({
+          name: state.name,
+          age: state.age,
+        });
+
+        state.status = `Friend ${state.name}
+          successfully added. Got id ${id}`;
+        reset()
+        initList()
+      } catch (error) {
+        state.status = `Failed to add
+          ${state.name}: ${error}`;
+      }
+    });
+  };
+  const reset = () => {
+    resetFields();
+  };
+
+  const initList = async() => {
+    const data:List[] = await db.friends.toArray()
+    state.list = data
+  }
+</script>
+
+<style scoped>
+  .skeleton-demo {
+    border: 1px solid #f4f4f4;
+  }
+</style>
