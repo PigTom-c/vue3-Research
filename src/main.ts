400b1b352a466240325d65c785e0e4c552bd0c18
csj <sjchen@gk-estor.com>
2022-08-11 16:58:35 +0800

    feat: 'pinia'

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue';
 
 import Antd from 'ant-design-vue';
 import { setupRouter } from './router';
-
 import 'ant-design-vue/dist/antd.css';
 import '/@/assets/css/tailwind.css';
 import 'tailwindcss/tailwind.css';
@@ -11,6 +10,7 @@ import 'tailwindcss/tailwind.css';
 import { setupGlobDirectives } from './utils/instructions/index';
 import { setupAutoAnimate } from '/@/directive/autoAnimate/index';
 import VueHighcharts from '/@/directive/highcharts';
+import { setupStore } from '/@/store';
 
 if (import.meta.env.DEV) {
   import('vuedraggable');
@@ -20,6 +20,7 @@ if (import.meta.env.DEV) {
 async function bootstrap() {
   const app = createApp(App);
   setupRouter(app);
+  setupStore(app);
   setupAutoAnimate(app);
   setupGlobDirectives(app);
   app.use(VueHighcharts).use(Antd).mount('#app');
---
+++ src/views/pinia/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="text-5xl">pinia</div>
+    <div>{{ count }}</div>
+    <a-button @click="handleClick">click me</a-button>
+  </div>
+</template>
+<script setup lang="ts">
+  import { onMounted, computed } from 'vue';
+  import useStore from '/@/store/modules/user';
+
+  const counter = useStore();
+
+  const count = computed(() => counter.count);
+
+  const handleClick = () => {
+    counter.count++;
+  };
+</script>
+<style scoped></style>
