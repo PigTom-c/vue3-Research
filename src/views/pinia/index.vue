da278e09ae6ad386467623c7eec6622d3759f68e
csj <sjchen@gk-estor.com>
2022-08-17 14:57:50 +0800

    feat: 'base64'

--- src/views/pinia/index.vue
+++ src/views/pinia/index.vue
@@ -6,12 +6,21 @@
   </div>
 </template>
 <script setup lang="ts">
-  import { onMounted, computed } from 'vue';
+  import { ref, onMounted, computed } from 'vue';
   import useStore from '/@/store/modules/user';
+  import { utoa, atou } from '/@/utils/file/base64'
 
   const counter = useStore();
 
   const count = computed(() => counter.count);
+
+  onMounted(() => {
+    let utoa1 = utoa('大睡觉了看大事哦dasdsaW@!!**(');
+    let atou1 = atou(utoa1);
+    console.log(utoa1);
+    console.log(atou1);
+
+  })
 
   const handleClick = () => {
     counter.count++;
